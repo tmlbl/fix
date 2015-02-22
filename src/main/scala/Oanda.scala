cc20dfbe01ae82800a082284695da8efc2cc5060
Tim Lebel <tim.lebel@gmail.com>
2015-02-22 14:06:05 -0800

    spray still sucks and is not workingg!

--- src/main/scala/Oanda.scala
+++ src/main/scala/Oanda.scala
@@ -26,9 +26,11 @@ object Oanda {
   implicit val CandleFormat = new RootJsonFormat[Candle] {
     def read(json: JsValue): Candle = {
       println("Candle data class name is", json.getClass.toString)
+      println(json.compactPrint)
       val fields = json.asJsObject.getFields("time")
       fields match {
         case Seq(JsString(time)) => new Candle(time = rfcDate.parse(time), 0, 0, 0, 0, 0, false)
+        case _ => throw new DeserializationException("Match failed")
       }
     }
     def write(candle: Candle) = JsObject(
@@ -42,6 +44,16 @@ object Oanda {
     )
   }
 
+  implicit val SeqCandleFormat = new RootJsonFormat[Seq[Candle]] {
+    def read(json: JsValue): Seq[Candle] = {
+      json.asJsObject.getFields().map((field: JsValue) => {
+        println(field)
+        field.convertTo[Candle]
+      })
+    }
+    def write(candles: Seq[Candle]): JsValue = candles.toJson
+  }
+
   def getCandles(instrument: String, start: Date, end: Date): Unit = {
     val res = Http(baseUrl + "/v1/candles").params(Seq(
       "instrument" -> instrument,
@@ -51,6 +63,6 @@ object Oanda {
     ))
     val resJson = res.asString.body.parseJson
     val candles = resJson.asJsObject.getFields("candles")
-    val parsed = candles.map((c:JsValue) => c.convertTo[Candle])
+    candles.foreach((candleList: JsValue) => candleList.convertTo[Seq[Candle]])
   }
 }
